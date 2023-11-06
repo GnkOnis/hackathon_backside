82fd58ee9e0538ec5b423235e05a9a2bb2ac46f6
genki <gensan.onis@gmail.com>
2023-11-06 22:06:06 +0900

    handler_table completed

--- main.go
+++ main.go
@@ -11,7 +11,6 @@ import (
 	"log"
 	_ "math/rand"
 	"net/http"
-	"os"
 	"time"
 	_ "time"
 )
@@ -23,6 +22,7 @@ type SendJson struct {
 	Link       string `json:"link"`
 	CreateTime string `json:"createtime"`
 	UpdateTime string `json:"updatetime"`
+	Numcomment int    `json:"numcomment"`
 	Summary    string `json:"summary"`
 	Name       string `json:"name"`
 }
@@ -31,18 +31,18 @@ var db *sql.DB
 
 func init() {
 	//デプロイ用
-	mysqlUser := os.Getenv("MYSQL_USER")
-	mysqlPwd := os.Getenv("MYSQL_PWD")
-	mysqlHost := os.Getenv("MYSQL_HOST")
-	mysqlDatabase := os.Getenv("MYSQL_DATABASE")
-	connStr := fmt.Sprintf("%s:%s@%s/%s", mysqlUser, mysqlPwd, mysqlHost, mysqlDatabase)
-	_db, err := sql.Open("mysql", connStr)
+	//mysqlUser := os.Getenv("MYSQL_USER")
+	//mysqlPwd := os.Getenv("MYSQL_PWD")
+	//mysqlHost := os.Getenv("MYSQL_HOST")
+	//mysqlDatabase := os.Getenv("MYSQL_DATABASE")
+	//connStr := fmt.Sprintf("%s:%s@%s/%s", mysqlUser, mysqlPwd, mysqlHost, mysqlDatabase)
+	//_db, err := sql.Open("mysql", connStr)
 
-	////mysqlのコンテナに接続
-	//sqluser := "test_user"
-	//sqlpwd := "password"
-	//sqldatabase := "test_database"
-	//_db, err := sql.Open("mysql", fmt.Sprintf("%s:%s@(localhost:3306)/%s", sqluser, sqlpwd, sqldatabase))
+	//mysqlのコンテナに接続
+	sqluser := "test_user"
+	sqlpwd := "password"
+	sqldatabase := "test_database"
+	_db, err := sql.Open("mysql", fmt.Sprintf("%s:%s@(localhost:3306)/%s", sqluser, sqlpwd, sqldatabase))
 
 	if err != nil {
 		log.Fatalf("fail: sql.Open, %v\n", err)
@@ -62,7 +62,7 @@ func handler_table(w http.ResponseWriter, r *http.Request) {
 
 		var categoryStr string
 		if category == "0" {
-			categoryStr = "1,2,3,"
+			categoryStr = "1,2,3"
 		} else {
 			categoryStr = category
 		}
@@ -88,7 +88,9 @@ func handler_table(w http.ResponseWriter, r *http.Request) {
 			return
 		}
 		query := "SELECT * FROM maintable WHERE category IN (" + categoryStr + ") AND curr IN (" + currStr + ") " + orderStr
-
+		////////////////////////////////
+		fmt.Println(query)
+		//////////////////////////////
 		rows, err := db.Query(query)
 		if err != nil {
 			http.Error(w, err.Error(), http.StatusInternalServerError)
@@ -99,7 +101,7 @@ func handler_table(w http.ResponseWriter, r *http.Request) {
 		items := []SendJson{}
 		for rows.Next() {
 			var item SendJson
-			err := rows.Scan(&item.Title, &item.Category, &item.Curr, &item.Link, &item.CreateTime, &item.UpdateTime, &item.Summary, &item.Name)
+			err := rows.Scan(&item.Title, &item.Category, &item.Curr, &item.Link, &item.CreateTime, &item.UpdateTime, &item.Numcomment, &item.Summary, &item.Name)
 			if err != nil {
 				http.Error(w, err.Error(), http.StatusInternalServerError)
 				return
@@ -129,17 +131,76 @@ func handler_table(w http.ResponseWriter, r *http.Request) {
 			return
 		}
 		nowTime := time.Now()
-		query := "INSERT INTO maintable (title,category,curr,link,createtime,updatetime,comment,name) VALUES(?,?,?,?,?,?,?,?)"
-		_, err = db.Exec(query, Recievejson.Title, Recievejson.Category, Recievejson.Curr, Recievejson.Link, nowTime, nowTime, Recievejson.Comment, Recievejson.Name)
+		query := "INSERT INTO maintable (title,category,curr,link,createtime,updatetime,numcomment,summary,name) VALUES(?,?,?,?,?,?,?,?,?)"
+		_, err = db.Exec(query, Recievejson.Title, Recievejson.Category, Recievejson.Curr, Recievejson.Link, nowTime, nowTime, 1, "まだsummaryは実装されていません", Recievejson.Name)
 		if err != nil {
 			http.Error(w, err.Error(), http.StatusInternalServerError)
 			return
 		}
 		w.WriteHeader(http.StatusCreated)
 	case http.MethodPut:
+		var Recievejson struct {
+			Title    string `json:"title"`
+			Category int    `json:"category"`
+			Curr     int    `json:"curr"`
+			Link     string `json:"link"`
+			Name     string `json:"name"`
+		}
+		err := json.NewDecoder(r.Body).Decode(&Recievejson)
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+		if Recievejson.Category != 100 {
+			_, err = db.Exec("UPDATE maintable SET category = ? WHERE title = ?", Recievejson.Category, Recievejson.Title)
+		}
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+		if Recievejson.Curr != 100 {
+			_, err = db.Exec("UPDATE maintable SET curr = ? WHERE title = ?", Recievejson.Curr, Recievejson.Title)
+		}
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+		if Recievejson.Link != "nochange" {
+			_, err = db.Exec("UPDATE maintable SET link = ? WHERE title = ?", Recievejson.Link, Recievejson.Title)
+		}
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+		if Recievejson.Name != "nochange" {
+			_, err = db.Exec("UPDATE maintable SET name = ? WHERE title = ?", Recievejson.Name, Recievejson.Title)
+		}
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+		currentTime := time.Now()
+		_, err = db.Exec("UPDATE maintable SET updatetime = ? WHERE title = ?", currentTime, Recievejson.Title)
+
+		w.WriteHeader(http.StatusNoContent)
 	case http.MethodDelete:
+		title := r.URL.Query().Get("title")
+		if title == "" {
+			http.Error(w, "Title is required", http.StatusBadRequest)
+		}
+		query := "DELETE FROM maintable WHERE title = ?"
+		_, err := db.Exec(query, title)
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
 	}
 }
+
 func handler_element(w http.ResponseWriter, r *http.Request) {
 	switch r.Method {
 	case http.MethodGet:
@@ -147,7 +208,6 @@ func handler_element(w http.ResponseWriter, r *http.Request) {
 	case http.MethodDelete:
 	}
 }
-
 
 func main() {
 	http.HandleFunc("/table", handler_table)
@@ -160,7 +220,7 @@ func main() {
 	})
 	handlerWithCors := c.Handler(http.DefaultServeMux)
 	log.Println("Listening...")
-	if err := http.ListenAndServe(":8080", handlerWithCors); err != nil {
+	if err := http.ListenAndServe(":8000", handlerWithCors); err != nil {
 		log.Fatal(err)
 	}
 }
